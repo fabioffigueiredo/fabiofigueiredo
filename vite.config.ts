4b533b8831fc76948f39b02ca28b7d561c6a72e4
fabioffigueiredo <32468093+fabioffigueiredo@users.noreply.github.com>
2025-10-15 20:15:18 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/fabiofigueiredo/' : './',
   server: {
     port: 8081,
   },
